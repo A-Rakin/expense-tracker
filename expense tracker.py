6d815384d8eb69899fba5e7725c0f251b824969b
Abrar Rakin <rakinabrar151@gmail.com>
2025-10-04 13:12:40 +0600

    load the expenses

--- expense tracker.py
+++ expense tracker.py
@@ -1,5 +1,7 @@
 import csv
 from datetime import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def log_expense(date, category, amount, description):
@@ -8,5 +10,8 @@ def log_expense(date, category, amount, description):
 		writer.writerow([date, category, amount, description])
 
 # Example
-log_expense(datetime.now().strftime("%Y-%m-%d"), "Food", 200.00, "Transport to cafe")
-print("Expense Logged!")
+#log_expense(datetime.now().strftime("%Y-%m-%d"), "Food", 200.00, "Transport to cafe")
+#print("Expense Logged!")
+
+def load_expenses():
+	return pd.read_csv("expenses.csv", names=["Date", "Category", "Amount", "Description"])
